bc80345deb12657097e5cd073fdae88ebd5dfbc6
Kamranbb <kamrannb@code.edu.az>
2024-03-31 00:48:49 +0400

    Proje dosyası ekle.

---
+++ HomeWorkDelegate/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace HomeWorkDelegate.Entities
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Addres { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id {Id} Salary {Salary}";
+        }
+        //public override bool Equals(object? obj)
+        //{
+        //    if (obj == null|| obj is not Person) return false;
+        //    Person person = obj as Person;
+        //    return person.Name.Equals(Name);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return Name.GetHashCode();
+        //}
+    }
+}
---
+++ HomeWorkDelegate/Program.cs
@@ -0,0 +1,53 @@
+﻿using HomeWorkDelegate.Entities;
+
+namespace HomeWorkDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person person2 = new Person();
+            person2.Id= Guid.NewGuid();  
+            person2.Name = "Test";
+            person2.Salary = 2384;
+           Person person1 = new Person();
+            person1.Id= Guid.NewGuid();
+            person1.Name = "Test";
+            person1.Salary = 1233;
+
+            
+
+            List<Person> person = new List<Person>() {person2,person1};
+            foreach (var item in CalculateSalary(p=>p.Salary>1000&&p.Salary<2000,person))
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Person> CalculateSalary (Predicate<Person> predicate,List<Person> person)
+            {
+                return person.FindAll(predicate);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine(person.Equals(person1));
+            //HashSet<Person> list = new HashSet<Person>();   
+            //list.Add(person);
+            //list.Add(person1);
+            //Console.WriteLine(list.Count);
+        }
+
+    }
+}
