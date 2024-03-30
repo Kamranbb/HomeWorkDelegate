90d92546bafea7b810f13f6ffd2a187015ce2cb7
Kamranbb <kamrannb@code.edu.az>
2024-03-31 01:02:45 +0400

    added Task2

---
+++ HomeWorkDelegate/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWorkDelegate.Entities
+{
+    internal class Book
+    {
+        public Guid Id { get; set; }    
+        public string Name { get; set; }    
+        public string  AuthorName { get; set; } 
+        public int PageCount { get; set; }  
+    }
+}
--- HomeWorkDelegate/Program.cs
+++ HomeWorkDelegate/Program.cs
@@ -6,7 +6,8 @@ namespace HomeWorkDelegate
     {
         static void Main(string[] args)
         {
-           Person person2 = new Person();
+            #region Task1
+            Person person2 = new Person();
             person2.Id= Guid.NewGuid();  
             person2.Name = "Test";
             person2.Salary = 2384;
@@ -17,36 +18,43 @@ namespace HomeWorkDelegate
 
             
 
-            List<Person> person = new List<Person>() {person2,person1};
-            foreach (var item in CalculateSalary(p=>p.Salary>1000&&p.Salary<2000,person))
-            {
-                Console.WriteLine(item);
-            }
+            //List<Person> person = new List<Person>() {person2,person1};
+            //foreach (var item in CalculateSalary(p=>p.Salary>1000&&p.Salary<2000,person))
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            List<Person> CalculateSalary (Predicate<Person> predicate,List<Person> person)
-            {
-                return person.FindAll(predicate);
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            //List<Person> CalculateSalary (Predicate<Person> predicate,List<Person> person)
+            //{
+            //    return person.FindAll(predicate);
+            //}
 
             //Console.WriteLine(person.Equals(person1));
             //HashSet<Person> list = new HashSet<Person>();   
             //list.Add(person);
             //list.Add(person1);
             //Console.WriteLine(list.Count);
+            #endregion
+            #region Task2 
+            Book book = new Book();
+            book.Id = Guid.NewGuid();
+            book.Name = "Test";
+            book.AuthorName = "Nizami";
+            Book book2 = new Book();    
+            book2.Id = Guid.NewGuid();
+            book2.Name = "Test1";
+            book2.AuthorName = "Nizami";
+            List<Book> books = new();
+            books.Add(book);
+            books.Add(book2);
+            //Console.WriteLine(FindBooksOfNizami(b => b.AuthorName == "Nizami", books));
+
+            //int FindBooksOfNizami (Predicate<Book> predicate,List<Book> list)
+            //{
+            //    return list.FindAll(predicate).Count;
+            //}
+
+            #endregion 
         }
 
     }
